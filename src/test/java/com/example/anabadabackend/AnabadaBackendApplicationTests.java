85ae65b8c433660d1bf1235f6b2114bde8925ca5
junsu <s26022@gsm.hs.kr>
2026-05-19 23:21:48 +0900

    init

---
+++ src/test/java/com/example/anabadabackend/AnabadaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anabadabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabadaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
