2ab31830fe25993abafe700405b7652c2f401b9e
junsu <s26022@gsm.hs.kr>
2026-06-15 23:01:55 +0900

    스웨거 수정 및 코드리뷰

--- src/main/java/com/example/anabadabackend/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/anabadabackend/global/security/JwtAuthenticationFilter.java
@@ -23,15 +23,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter  {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String path = request.getRequestURI();
-
+        String path = request.getServletPath();
         return path.startsWith("/api/auth/email")
                 || path.startsWith("/api/auth/signin")
-                || path.startsWith("/api/auth/signup")
-                || path.startsWith("/swagger-ui")
-                || path.startsWith("/v3/api-docs");
-    }
+                || path.startsWith("/api/auth/signup") //
+                || path.startsWith("/swagger-ui") //
+                || path.startsWith("/v3/api-docs"); //
 
+    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
